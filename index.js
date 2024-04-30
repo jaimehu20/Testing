a24e01cc844ad216394d1ab1137b135652954719
jaimehu20 <jaimehu20@gmail.com>
2024-04-30 17:40:39 +0200

    ADD/ room booking class and addBooking method

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-function suma(a,b){
-    return a + b;
-}
+class Room  {
+    constructor({name, bookings, rate, discount}){
+        this.name = name;
+        this.bookings = [];
+        this.rate = rate;
+        this.discount = discount
+    }
+    addBooking({checkIn, checkOut, guestName}){
+        this.bookings.push({checkIn : checkIn, checkOut: checkOut, guestName: guestName});
+    }
+};
 
-module.exports = suma;
+
+class Booking {
+    constructor({name, email, checkin, checkout, discount, room}){
+        this.name = name;
+        this.email = email;
+        this.checkin = checkin;
+        this.checkout = checkout;
+        this.discount = discount;
+        this.room = room;
+    }
+};
+
+module.exports = {Room, Booking};
--- index.test.js
+++ index.test.js
@@ -1,5 +1,31 @@
-const suma = require('./index');
+const { Room, Booking } = require('./index')
 
-test('1 + 2 = 3', () => {
-    expect(suma(1, 2)).toBe(3);
-});
+function rangeDates(start, end) {
+    let startDate = new Date(start);
+    let endDate = new Date(end)
+    for(let day=startDate; day<=endDate; day.setDate(day.getDate()+1)) {
+        console.log(day.toLocaleDateString());
+    }
+}
+
+test("nombre del cliente es Juan", () => {
+    const Room1 = new Room({
+        name: "Habitacion 112",
+        bookings: [],
+        rate: 100,
+        discount: 20
+    })
+    Room1.addBooking({checkIn: "2024/04/10", checkOut: "2024/04/15", guestName: "Juan"})
+    expect(Room1.bookings[0].guestName === "Juan").toBeTruthy();
+})
+
+test("nombre del cliente es Juan", () => {
+    const Room1 = new Room({
+        name: "Habitacion 112",
+        bookings: [],
+        rate: 100,
+        discount: 20
+    })
+    Room1.addBooking({checkIn: "2024/04/10", checkOut: "2024/04/15", guestName: "Alberto"})
+    expect(Room1.bookings[0].guestName === "Juan").toBeFalsy();
+})
