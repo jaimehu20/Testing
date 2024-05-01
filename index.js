ad4ffc8e926632fc5f9be1f83787175f1ab64698
jaimehu20 <jaimehu20@gmail.com>
2024-05-01 20:38:34 +0200

    ADD/ occupancy percentage method

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+function rangeDates(start, end) {
+    let startDate = new Date(start);
+    let endDate = new Date(end)
+    let range = []
+    for(let day=startDate; day<endDate; day.setDate(day.getDate()+1)) {
+        range.push(day.toISOString().slice(0,10));
+    }
+    return range
+}
+
 class Room  {
     constructor({name, bookings, rate, discount}){
         this.name = name;
@@ -5,10 +15,34 @@ class Room  {
         this.rate = rate;
         this.discount = discount
     }
-    addBooking({checkIn, checkOut, guestName}){
-        this.bookings.push({checkIn : checkIn, checkOut: checkOut, guestName: guestName});
+    addBooking({checkIn, checkOut, guestName, room}){
+        this.bookings.push({checkIn : checkIn, checkOut: checkOut, guestName: guestName, room: room});
     }
-};
+    isOcuppied(date){
+        for (let i = 0; i < this.bookings.length; i++){
+            if (date >= this.bookings[i].checkIn && date < this.bookings[i].checkOut)
+            return true
+        }
+        return false
+    }
+    occupancyPercentage(startDate, endDate){
+        let daysOccupied = 0;
+        let dateRange = rangeDates(startDate, endDate);
+        let percent;
+        let prueba = [];
+        let newArray;
+
+        this.bookings.map((date) => {
+            prueba.push(rangeDates(date.checkIn, date.checkOut).flat());
+        })
+        newArray = prueba.flat()
+        newArray.map((date) => {if (dateRange.includes(date)) daysOccupied++})
+        percent = ((daysOccupied / dateRange.length) * 100).toFixed(0);
+        return percent 
+} 
+
+}
+
 
 
 class Booking {
@@ -22,4 +56,6 @@ class Booking {
     }
 };
 
+
 module.exports = {Room, Booking};
+
--- index.test.js
+++ index.test.js
@@ -8,24 +8,136 @@ function rangeDates(start, end) {
     }
 }
 
-test("nombre del cliente es Juan", () => {
-    const Room1 = new Room({
-        name: "Habitacion 112",
-        bookings: [],
-        rate: 100,
-        discount: 20
+describe("Is Occupied Method - Room test", () => {
+    test("Is not occupied, looking before first check in", () => {
+        const Room1 = new Room({
+            name: "Habitacion 112",
+            bookings: [],
+            rate: 100,
+            discount: 20
+        })
+        Room1.addBooking({checkIn: "2024/04/10", checkOut: "2024/04/15", guestName: "Juan"})
+        Room1.addBooking({checkIn: "2024/04/20", checkOut: "2024/04/23", guestName: "Fernando"})
+        Room1.addBooking({checkIn: "2024/05/01", checkOut: "2024/05/20", guestName: "Alejandro"})
+        expect(Room1.isOcuppied("2023/04/09")).toBe(false);
     })
-    Room1.addBooking({checkIn: "2024/04/10", checkOut: "2024/04/15", guestName: "Juan"})
-    expect(Room1.bookings[0].guestName === "Juan").toBeTruthy();
+    
+    test("Is not occupied, looking between two bookings, different day", () => {
+        const Room1 = new Room({
+            name: "Habitacion 112",
+            bookings: [],
+            rate: 100,
+            discount: 20
+        })
+        Room1.addBooking({checkIn: "2024/04/10", checkOut: "2024/04/15", guestName: "Juan"})
+        Room1.addBooking({checkIn: "2024/04/20", checkOut: "2024/04/23", guestName: "Fernando"})
+        Room1.addBooking({checkIn: "2024/05/01", checkOut: "2024/05/20", guestName: "Alejandro"})
+        expect(Room1.isOcuppied("2024/04/17")).toBe(false);
+    })
+    
+    test("Is not occupied, looking between two bookings, same day", () => {
+        const Room1 = new Room({
+            name: "Habitacion 112",
+            bookings: [],
+            rate: 100,
+            discount: 20
+        })
+        Room1.addBooking({checkIn: "2024/04/10", checkOut: "2024/04/15", guestName: "Juan"})
+        Room1.addBooking({checkIn: "2024/04/20", checkOut: "2024/04/23", guestName: "Fernando"})
+        Room1.addBooking({checkIn: "2024/05/01", checkOut: "2024/05/20", guestName: "Alejandro"})
+        expect(Room1.isOcuppied("2024/04/15")).toBe(false);
+    })
+    
+    test("Is occupied, first day of first booking", () => {
+        const Room1 = new Room({
+            name: "Habitacion 112",
+            bookings: [],
+            rate: 100,
+            discount: 20
+        })
+        Room1.addBooking({checkIn: "2024/04/10", checkOut: "2024/04/15", guestName: "Juan"})
+        Room1.addBooking({checkIn: "2024/04/20", checkOut: "2024/04/23", guestName: "Fernando"})
+        Room1.addBooking({checkIn: "2024/05/01", checkOut: "2024/05/20", guestName: "Alejandro"})
+        expect(Room1.isOcuppied("2024/04/10")).toBe(true);
+    })
+    
+    test("Is occupied, first day of last booking", () => {
+        const Room1 = new Room({
+            name: "Habitacion 112",
+            bookings: [],
+            rate: 100,
+            discount: 20
+        })
+        Room1.addBooking({checkIn: "2024/04/10", checkOut: "2024/04/15", guestName: "Juan"})
+        Room1.addBooking({checkIn: "2024/04/20", checkOut: "2024/04/23", guestName: "Fernando"})
+        Room1.addBooking({checkIn: "2024/05/01", checkOut: "2024/05/20", guestName: "Alejandro"})
+        expect(Room1.isOcuppied("2024/05/01")).toBe(true);
+    })
+    
+    test("Is occupied, middle day of first booking", () => {
+        const Room1 = new Room({
+            name: "Habitacion 112",
+            bookings: [],
+            rate: 100,
+            discount: 20
+        })
+        Room1.addBooking({checkIn: "2024/04/10", checkOut: "2024/04/15", guestName: "Juan"})
+        Room1.addBooking({checkIn: "2024/04/20", checkOut: "2024/04/23", guestName: "Fernando"})
+        Room1.addBooking({checkIn: "2024/05/01", checkOut: "2024/05/20", guestName: "Alejandro"})
+        expect(Room1.isOcuppied("2024/04/13")).toBe(true);
+    })
+    
+    test("Is occupied, middle day of last booking", () => {
+        const Room1 = new Room({
+            name: "Habitacion 112",
+            bookings: [],
+            rate: 100,
+            discount: 20
+        })
+        Room1.addBooking({checkIn: "2024/04/10", checkOut: "2024/04/15", guestName: "Juan"})
+        Room1.addBooking({checkIn: "2024/04/20", checkOut: "2024/04/23", guestName: "Fernando"})
+        Room1.addBooking({checkIn: "2024/05/01", checkOut: "2024/05/20", guestName: "Alejandro"})
+        expect(Room1.isOcuppied("2024/05/10")).toBe(true);
+    })
 })
 
-test("nombre del cliente es Juan", () => {
-    const Room1 = new Room({
-        name: "Habitacion 112",
-        bookings: [],
-        rate: 100,
-        discount: 20
+describe("Occupancy Percentage Method - Room Test", () => {
+    test("Occupancy 100%", () => {
+        const Room1 = new Room({
+            name: "Habitacion 112",
+            bookings: [],
+            rate: 100,
+            discount: 20
+        })
+        Room1.addBooking({checkIn: "2024/04/10", checkOut: "2024/04/15", guestName: "Juan", Room: Room1})
+        Room1.addBooking({checkIn: "2024/04/20", checkOut: "2024/04/23", guestName: "Fernando", Room: Room1})
+        Room1.addBooking({checkIn: "2024/05/01", checkOut: "2024/05/20", guestName: "Alejandro", Room: Room1})
+        expect(Room1.occupancyPercentage("2024/04/10", "2024/04/15")).toBe("100");
+    });
+    
+    test("Occupancy 0%", () => {
+        const Room1 = new Room({
+            name: "Habitacion 112",
+            bookings: [],
+            rate: 100,
+            discount: 20
+        })
+        Room1.addBooking({checkIn: "2024/04/10", checkOut: "2024/04/15", guestName: "Juan", Room: Room1})
+        Room1.addBooking({checkIn: "2024/04/20", checkOut: "2024/04/23", guestName: "Fernando", Room: Room1})
+        Room1.addBooking({checkIn: "2024/05/01", checkOut: "2024/05/20", guestName: "Alejandro", Room: Room1})
+        expect(Room1.occupancyPercentage("2023/04/10", "2023/04/15")).toBe("0");
     })
-    Room1.addBooking({checkIn: "2024/04/10", checkOut: "2024/04/15", guestName: "Alberto"})
-    expect(Room1.bookings[0].guestName === "Juan").toBeFalsy();
+    
+    test("Occupancy 50%", () => {
+        const Room1 = new Room({
+            name: "Habitacion 112",
+            bookings: [],
+            rate: 100,
+            discount: 20
+        })
+        Room1.addBooking({checkIn: "2024/04/10", checkOut: "2024/04/15", guestName: "Juan", Room: Room1})
+        Room1.addBooking({checkIn: "2024/04/20", checkOut: "2024/04/23", guestName: "Fernando", Room: Room1})
+        Room1.addBooking({checkIn: "2024/05/01", checkOut: "2024/05/20", guestName: "Alejandro", Room: Room1})
+        expect(Room1.occupancyPercentage("2024/04/10", "2024/04/20")).toBe("50");
+    })
 })
